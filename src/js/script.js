5b9160e93a8c3b9444e6d727ca7c319d23bd81e1
Pierre-Olivier Croteau <paolivier98@videotron.ca>
2021-02-23 15:33:09 -0500

    oii

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //Un commentaire
     console.log("Ça fonctionne!!!");
 
+    var hamburger = document.querySelector('.hamburger-animated');
+    var menMobile = document.querySelector(".nav-primary-mobile")
+
+
+    hamburger.addEventListener('click', openMenu);
+
+    function openMenu(evt) {
+        evt.preventDefault();
+
+        var cible = evt.currentTarget;
+
+        if(cible.classList.contains('open')){
+            cible.classList.remove("open");
+            menMobile.classList.remove('open')
+        }else{
+            cible.classList.add("open");
+            menMobile.classList.add("open");
+
+        }
+    }
+
 });
+
